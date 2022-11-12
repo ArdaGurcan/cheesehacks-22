e90dd95b965638d6227c51b9581ae87a8c79a3bf
11Rachelle11 <42658403+11Rachelle11@users.noreply.github.com>
2022-11-12 15:58:49 -0600

    map

---
+++ map/map.js
@@ -0,0 +1,56 @@
+import {Feature, Map, Overlay, View} from 'ol/index';
+import {OSM, Vector as VectorSource} from 'ol/source';
+import {Point} from 'ol/geom';
+import {Tile as TileLayer, Vector as VectorLayer} from 'ol/layer';
+import {fromLonLat} from 'ol/proj';
+import Geolocation from 'ol/Geolocation';
+
+const view = new View({
+  center: [0, 0],
+  zoom: 15,
+})
+
+const feature = new Feature(new Point([0, 0]));
+
+const map = new Map({
+  target: 'map',
+  view: view,
+  layers: [
+    new TileLayer({
+      source: new OSM(),
+    }),
+    new VectorLayer({
+      source: new VectorSource({
+        features: [feature],
+      }),
+      style: {
+        'circle-radius': 9,
+        'circle-fill-color': 'red',
+      },
+    }),
+  ],
+});
+
+const geolocation = new Geolocation({
+  tracking: true,
+  trackingOptions: {
+    enableHighAccuracy: true,
+  },
+  projection: view.getProjection(),
+});
+
+geolocation.on('error', function (error) {
+  console.log("there's a problem here");
+});
+
+console.log(geolocation.getPosition());
+
+geolocation.on('change:position', function () {
+  const coordinates = geolocation.getPosition();
+  feature.setGeometry(coordinates ? new Point(coordinates) : null);
+  view.setCenter(coordinates);
+});
+
+function getLocation() {
+  return geolocation.getPosition();
+}
