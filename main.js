504d2decbb617d694930ef50dfcf7a580fce38f8
BreakfastNoo <111155645+BreakfastNoo@users.noreply.github.com>
2022-11-12 14:55:19 -0600

    remove getCities

--- main.js
+++ main.js
@@ -1,11 +1,3 @@
-// Get a list of cities from your database
-async function getCities(db) {
-  const citiesCol = collection(db, 'cities');
-  const citySnapshot = await getDocs(citiesCol);
-  const cityList = citySnapshot.docs.map(doc => doc.data());
-  return cityList;
-}
-
 function switchTo1() {
     removeActive();
     hideAll();
