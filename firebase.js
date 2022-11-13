ef59825ba12e0c8a186ca5276b1034fb07521067
11Rachelle11 <42658403+11Rachelle11@users.noreply.github.com>
2022-11-12 22:45:29 -0600

    map in projecgt

--- firebase.js
+++ firebase.js
@@ -102,7 +102,7 @@ function addUser(name) {
   })
 }
 
-async function getFriends(name) {
+export default async function getFriends(name) {
   let friends = []
   await getDocs(userRef)
     .then((snapshot) => {
